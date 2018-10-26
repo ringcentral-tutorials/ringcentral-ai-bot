3c69e88482253d085a2c317f3aede708c1829053
ZHAO Xudong <zxdong@gmail.com>
2018-10-26 09:38:58 +0800

    save

--- src/handler.js
+++ src/handler.js
@@ -4,5 +4,8 @@ global.Promise = require('bluebird')
 const hub = require('./lib/hub').default
 
 exports.bot = async (event) => {
+  console.log('----------event get--------------')
+  console.log(event)
+  console.log('-----------event get-------------')
   return await hub(event)
 }
