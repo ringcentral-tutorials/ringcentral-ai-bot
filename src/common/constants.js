15c5ff2977dad69406cd2240cb4285b0fef8e268
ZHAO Xudong <zxdong@gmail.com>
2018-10-30 11:03:31 +0800

    Edit readme, add SUBSCRIBE_EXPIRE env

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,2 +1,4 @@
 export const subscribeInterval = '/restapi/v1.0/subscription/~?threshold=59&interval=15'
-export const expiresIn = 1799
+export const expiresIn = process.env.SUBSCRIBE_EXPIRE
+  ? parseInt(process.env.SUBSCRIBE_EXPIRE)
+  : 1799
